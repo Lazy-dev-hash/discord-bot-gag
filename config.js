95b3616f8ed77604c8431e3f4da39242aac8a0e8
Lazy-dev-hash <nelzys52@gmail.com>
2025-06-06 12:05:35 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  botToken: "YOUR_DISCORD_BOT_TOKEN_HERE",
+  fetchIntervalMs: 10 * 1000, // 10 seconds interval for fetching stock updates
+  footerText: "Created by Sunnel",
+  prefix: "/", // command prefix if needed later
+};
