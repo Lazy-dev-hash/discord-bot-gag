d7c0b48808d9c0ecb9b24d4297bd97ae94644738
Lazy-dev-hash <nelzys52@gmail.com>
2025-06-06 12:22:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   botToken: "MTM4MDM3MjM0MTE0OTkyNTQyNw.GIVCrG.GztZGt1XalZmaXMDF8D8QT_0XkZ_G5OosN1pck",
-  fetchIntervalMs: 10 * 1000, // 10 seconds interval for fetching stock updates
-  footerText: "Created by Sunnel",
-  prefix: "/", // command prefix if needed later
+  guildId: "1380373829494505552",
+  prefix: "/", // optional if you use slash commands only
 };
